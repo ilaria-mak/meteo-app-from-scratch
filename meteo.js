31647926c8df2385b35721bbc32192204def9443
ilaria-mak <ilaria.laterza@gmail.com>
2024-02-10 17:49:28 +0000

    Formatted Date in weekdays

--- meteo.js
+++ meteo.js
@@ -4,14 +4,43 @@ function displayMeteoData(response) {
   let descriptionDisplayed = document.querySelector("#description");
   let humidityDisplayed = document.querySelector("#humidity");
   let windSpeedDisplayed = document.querySelector("#wind-speed");
+  let timeDisplayed = document.querySelector("#time");
+  let date = new Date(response.data.time * 1000); //prende l'elemento 'time' dall'API associato alla data di oggi che è però espresso in millisecondi dal 1970 (moltiplico dunque per 1000)
 
   //ogni volta che creo una variabile, la associo al rispettivo elemento in HTML che viene fatto corrispondere alle info prese dall'API
+  timeDisplayed.innerHTML = dateFormatted(date); //la variabile mi ritorna la data formattata (FINE STEP 6)
   temperatureDisplayed.innerHTML = Math.round(
     response.data.temperature.current
   ); //la variabile mi ritorna i valori dell'evento response già arrotondati
   descriptionDisplayed.innerHTML = response.data.condition.description; //la descrizione visualizzata verrà presa dall'API
-  humidityDisplayed.innerHTML = `${response.data.temperature.humidity}%`; //
+  humidityDisplayed.innerHTML = `${response.data.temperature.humidity}%`;
   windSpeedDisplayed.innerHTML = `${response.data.wind.speed} km/h`;
+}
+
+//STEP 6: Formatta la data
+function dateFormatted(date) {
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+  let weekdays = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ]; //siccome la funzione 'New Date' mi rimanda il gg sett sotto forma di numero da 0 a 6, creo un array di oggetti
+  let dayOfTheWeek = date.getDay(); //ottiene il giorno della settimana
+  let dayFormatted = weekdays[dayOfTheWeek]; //ottieni il nome del giorno della settimana
+
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  return `${dayFormatted} ${hours}:${minutes}`; //Restituisci il giorno della settimana seguito da ora e minuti
 }
 
 //STEP 2: chiama l'API
