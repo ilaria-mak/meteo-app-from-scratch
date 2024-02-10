f3caecc8b757ef760555b938eb4dd1bbd9c268af
ilaria-mak <ilaria.laterza@gmail.com>
2024-02-10 15:24:00 +0000

    Updated all weather details

--- meteo.js
+++ meteo.js
@@ -1,9 +1,17 @@
-//STEP 3:
+//STEP 3: Creo le variabili da associare agli id e agli elementi dell'API
 function displayMeteoData(response) {
   let temperatureDisplayed = document.querySelector("#temperature"); //chiama l'id per modificarlo
+  let descriptionDisplayed = document.querySelector("#description");
+  let humidityDisplayed = document.querySelector("#humidity");
+  let windSpeedDisplayed = document.querySelector("#wind-speed");
+
+  //ogni volta che creo una variabile, la associo al rispettivo elemento in HTML che viene fatto corrispondere alle info prese dall'API
   temperatureDisplayed.innerHTML = Math.round(
     response.data.temperature.current
   ); //la variabile mi ritorna i valori dell'evento response già arrotondati
+  descriptionDisplayed.innerHTML = response.data.condition.description; //la descrizione visualizzata verrà presa dall'API
+  humidityDisplayed.innerHTML = `${response.data.temperature.humidity}%`; //
+  windSpeedDisplayed.innerHTML = `${response.data.wind.speed} km/h`;
 }
 
 //STEP 2: chiama l'API
